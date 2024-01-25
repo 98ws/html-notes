63582575a096ebfca7c5c4462b9d9b40c808f054
98ws <ws0328@foxmail.com>
2024-01-25 15:49:12 +0800

    add favicon file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,6 +23,6 @@ language = 'zh'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+html_favicon = 'media/favicon.ico'
 html_theme = 'alabaster'
 html_static_path = ['_static']
