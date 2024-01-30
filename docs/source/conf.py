942d47cddaa92fc07993d32d5307257016d916b6
98ws <129597228+98ws@users.noreply.github.com>
2024-01-30 15:36:25 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,15 +6,19 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = '个人学习笔记'
-copyright = ''
+project = '学习笔记'
+copyright = 'Wang Shun'
 author = '王顺'
 release = '2024.01'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions += [
+   "sphinxawesome.highlighting",
+   # To help you with the upgrade to version 5:
+   # "sphinxawesome.deprecated",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +28,6 @@ language = 'zh'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 html_favicon = 'media/favicon.ico'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinxawesome_theme'
 html_static_path = ['_static']
+
